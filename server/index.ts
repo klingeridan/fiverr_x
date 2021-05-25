362cd424b601b5acbbe62d689cbc3e3f983e2b86
Idan Klinger <idan.k@fiverr.com>
2021-05-25 18:11:31 +0300

    adding some comment

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// SOME COMMENT
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-app/dist')));
